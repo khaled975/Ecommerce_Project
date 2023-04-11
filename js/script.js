045f8d183c3fd531d722a30d104c3f4b5959d16d
khaled975 <khaledbebo975@gmail.com>
2023-04-11 18:18:23 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const bar=document.querySelector(".mobile")
+const close=document.querySelector(".close")
+const nav=document.querySelector("header ul")
+if(bar){
+    bar.addEventListener("click",()=>{
+        nav.classList.add("active")
+    })
+}
+if(close){
+    close.addEventListener("click",()=>{
+        nav.classList.remove("active")
+    })
+}
+// get product details
+let mainImg= document.getElementById("mainImg")
+let selectedImg=document.querySelectorAll(".products .item img")
+
+selectedImg.forEach((item)=>{item.addEventListener("click",()=>{
+    // console.log(item)
+    window.location.href="productdetail.html"
+    mainImg.src=item.src
+})})
+
+// img carsoul
+let imgs= document.querySelectorAll(".product-imgs div img")
+imgs.forEach((item)=>{item.addEventListener("click",()=>{
+    mainImg.src=item.src
+})})
+
